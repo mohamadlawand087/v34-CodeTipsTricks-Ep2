258f31298a80d9e8c6004cd4cbb62fbc72a7206f
mohamad <mohamad.lawand@outlook.com>
2021-06-13 13:07:47 +0100

    initial commit

---
+++ ctte2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console; // This is a static directive
+
+PrintMyName("Mohamad");
+
+await Task.Delay(3000);
+
+WriteLine("It is breakfast time :)");
+
+WriteLine("Please input your email");
+var email = ReadLine(); 
+WriteLine($"Your email is {email}");
+
+
+static void PrintMyName(string name)
+{
+    WriteLine($"Your name is {name}");
+}
